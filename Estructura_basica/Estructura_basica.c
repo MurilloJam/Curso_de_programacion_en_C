4b2cb1d9ba1f6b02c0aefb2079534eeeaa5ac549
Gustavo Murillo <ga.murillogutierrez@ugto.mx>
2019-07-27 20:12:32 -0500

    add

---
+++ Estructura_basica/Estructura_basica.c
@@ -0,0 +1,21 @@
+/*  Esto es un comentario.
+        Cuando nosotros escribimos un texto entre los simbolos
+                        / *  * / 
+        El compilador los ignora y no los toma en cuenta
+        al construir nuestro programa.
+    Los comentarios son utiles para documentar nuestros
+    programas y saber que hace cada bloque determinado.
+    Los podemos colocar donde mas nos guste o sea conveniente.
+*/
+
+#include <stdio.h>
+int main(void)
+{
+    // En este bloque se inicializan las variables a utilizar 
+    
+    // En este bloque se indica al ordenador como realizar 
+    // las operaciones de nuestro programa
+
+    getchar();
+    return 0;
+}
