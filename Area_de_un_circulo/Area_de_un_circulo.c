968f4d4d7104423d8c9c3fbcd66fa90c9a154526
Gustavo Murillo <ga.murillogutierrez@ugto.mx>
2019-07-29 15:29:19 -0500

    Se agrego Area del circulo

---
+++ Area_de_un_circulo/Area_de_un_circulo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float radio = 10;
+    float pi = 3.1415;
+    float area;
+
+    area = pi * radio * radio; 
+    printf("El area de un circulo cuyo radio es %f es:\n%f\n", radio, area);
+}
